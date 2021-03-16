a50f816858940097ae06bbbe6af195c77ca719ed
lovelazur.dev.win <lovelazur.dev@gmail.com>
2021-03-16 18:46:50 +0900

    ComponentScan(basePackages= {"com.lovelazur", "com.lazur"})

--- src/main/java/com/lovelazur/controller/BoardController.java
+++ src/main/java/com/lovelazur/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.lovelazur.controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ComponentScan(basePackages= {"com.lovelazur", "com.lazur"})
 public class BoardController {
 	public BoardController() {
 		System.out.println("===> BoardController 생성");
